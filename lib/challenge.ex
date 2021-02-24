4c97f5db1d2e039c2184d8dfed3a0571575abe29
Guilherme Sernajoto <guilhermesernajoto@hotmail.com>
2021-02-24 19:11:28 -0300

    perf: reduced complexity from quadratic to linear

--- lib/challenge.ex
+++ lib/challenge.ex
@@ -3,13 +3,20 @@ defmodule Challenge do
     if Enum.empty?(emails) do
       raise "Email list is empty!"
     end
-    total = Enum.reduce(items, 0, fn(item, acc) -> item.price * item.qty + acc end)
+
+    total = Enum.reduce(items, 0, fn item, acc -> item.price * item.qty + acc end)
+
     emails = Enum.uniq(emails)
     qty_emails = length(emails)
     div = div(total, qty_emails)
     rem = rem(total, qty_emails)
-    emails_pay_more = if rem > 0, do: Enum.map(Enum.slice(emails, 0..rem-1), fn(email) -> %{"#{email}": div+1} end), else: []
-    emails_remaining = Enum.map(Enum.slice(emails, rem..qty_emails-1), fn(email) -> %{"#{email}": div} end)
-    Enum.reduce(emails_pay_more ++ emails_remaining, %{}, fn(map_item, acc) -> Map.merge(map_item, acc) end)
+
+    slice_emails = Enum.slice(emails, 0..rem-1)
+    emails_pay_more = if rem > 0, do: Enum.map(slice_emails, fn email -> %{"#{email}": div+1} end), else: []
+
+    slice_emails = Enum.slice(emails, rem..qty_emails-1)
+    emails_remaining = Enum.map(slice_emails, fn email -> %{"#{email}": div} end)
+
+    Enum.reduce(emails_pay_more ++ emails_remaining, %{}, fn map_item, acc -> Map.merge(map_item, acc) end)
   end
 end
